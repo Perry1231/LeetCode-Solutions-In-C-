1c640aae1c3bdef66a5a91b5b1fed1a15ba71d24
Vladyslav Vytrykush <148484877+Perry1231@users.noreply.github.com>
2026-07-09 20:33:52 +0300

    Update 14_longest_common_prefix.cpp - LeetCode Problem 14

--- 14_longest_common_prefix.cpp
+++ 14_longest_common_prefix.cpp
@@ -1,17 +1,42 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        string sum="";
-        int size=0;
+                string sum="";
+                int size=0;
         for (int i = 0; i < strs[0].size(); i++) {
             size++;
         }
-        for (int i=0; i<size-1; i++)
+
+        if (strs.empty()) return "";
+
+        else if (strs.size() == 1) return strs[0];
+
+        else if(strs.size() == 2)
         {
-            if(strs[0][i] == strs[1][i] &&strs[0][i] == strs[2][i])
+        for (int i=0; i<size; i++)
+        {
+            if(strs[0][i] == strs[1][i])
             sum += strs[1][i];
             else break;   
         }
- return sum;
+        }
+
+        else 
+        {
+           for (int i = 0; i < strs[0].size(); i++) 
+            {
+             char current_char = strs[0][i];
+            for(int j=0; j< strs.size(); j++)
+            {
+                if(strs[0][i] != strs[j][i] || i > strs[j].size())
+                {
+                return sum;
+                }
+            }
+            sum += current_char;
+        }
+        }
+         return sum;
+        
     }
 };
