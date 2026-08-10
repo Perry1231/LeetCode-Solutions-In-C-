29a30e3db9aee199774b671c9581888b2ce9a85e
Vladyslav Vytrykush <148484877+Perry1231@users.noreply.github.com>
2026-08-10 14:47:43 +0300

    Add 283_move_zeroes.cpp - LeetCode Problem 283

---
+++ 283_move_zeroes.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int size= nums.size();
+        int left =0;
+        for(int i=0; i<size; i++)
+        {
+            if(nums[i] != 0) 
+            {
+                swap(nums[left], nums[i]);
+            left++;
+            }
+        }
+    
+    }
+};
