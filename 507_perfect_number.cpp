5de3ea17c4b1c199ed95d9192697d762ef3ebc7a
Vladyslav Vytrykush <148484877+Perry1231@users.noreply.github.com>
2026-08-21 14:32:16 +0300

    Add 507_perfect_number.cpp - LeetCode Problem 507

---
+++ 507_perfect_number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool checkPerfectNumber(int num) {
+        int res=0;
+        for(int i=1; i<num; i++)
+        {
+            if(num % i == 0) res+=i;
+        }
+        if(num == res) return true;
+        else return false;
+    }
+};
