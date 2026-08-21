cb02b7df2c1a639595467e24b96bfa0245f3f296
Vladyslav Vytrykush <148484877+Perry1231@users.noreply.github.com>
2026-08-21 14:26:48 +0300

    Update 1013_fibonacci_number.cpp - LeetCode Problem 1013

--- 1013_fibonacci_number.cpp
+++ 1013_fibonacci_number.cpp
@@ -1,7 +1,14 @@
 class Solution {
 public:
     int fib(int n) {
+        int num1=0, num2=1, res=0;
         if(n == 0 || n==1) return n;
-        return fib(n-1) + fib(n-2);
+        for(int i=0 ;i< n-1; i++)
+        {
+            res = num1 + num2;
+            num1 = num2;
+            num2 = res;
+        }
+        return res;
     }
 };
