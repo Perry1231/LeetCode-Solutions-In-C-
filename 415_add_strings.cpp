5d0db9b39851fc1f88dd88013195d1228469f13f
Vladyslav Vytrykush <148484877+Perry1231@users.noreply.github.com>
2026-08-22 23:13:20 +0300

    Add 415_add_strings.cpp - LeetCode Problem 415

---
+++ 415_add_strings.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    string addStrings(string num1, string num2) {
+
+        int nums1 = std::stoi(num1);
+        int nums2 = std::stoi(num2);
+         return nums1 + nums2;
+    }
+};
