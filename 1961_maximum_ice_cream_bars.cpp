48f0e0a7c3b345d918485630e2f573f955d5aeb2
Vladyslav Vytrykush <148484877+Perry1231@users.noreply.github.com>
2026-08-27 22:10:22 +0300

    Update 1961_maximum_ice_cream_bars.cpp - LeetCode Problem 1961

--- 1961_maximum_ice_cream_bars.cpp
+++ 1961_maximum_ice_cream_bars.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     int maxIceCream(vector<int>& costs, int coins) {
         int res=0;
-    std::sort(coins.begin(), coins.end());
-    for(int i=0 i<costs.size(); i++)
+    std::sort(costs.begin(), costs.end());
+    for(int i=0; i<costs.size(); i++)
 {
 if(coins - costs[i] >=0) {coins -= costs[i]; res++;}
 else break;
