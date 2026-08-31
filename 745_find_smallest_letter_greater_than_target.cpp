38cc45f15a2ff98406cb0d6f06443fdd145494b8
Vladyslav Vytrykush <148484877+Perry1231@users.noreply.github.com>
2026-08-31 17:25:42 +0300

    Update 745_find_smallest_letter_greater_than_target.cpp - LeetCode Problem 745

--- 745_find_smallest_letter_greater_than_target.cpp
+++ 745_find_smallest_letter_greater_than_target.cpp
@@ -3,11 +3,7 @@ public:
     char nextGreatestLetter(vector<char>& letters, char target) {
        
        
-       for(int i=0; i<letters.size(); i++)
-       {
-        if(letters[i] > target) return letters[i];
-       }
-
-    return letters[0];
+      auto ub = std::upper_bound(letters.begin(), letters.end(), target);
+      return (ub != letters.end()) ? *ub : letters[0];
     }
 };
