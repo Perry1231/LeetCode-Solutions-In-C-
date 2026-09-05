9f4192dcc585f43eedcf6042053d8feaa3089515
Vladyslav Vytrykush <148484877+Perry1231@users.noreply.github.com>
2026-09-05 17:31:54 +0300

    Update 228_summary_ranges.cpp - LeetCode Problem 228

--- 228_summary_ranges.cpp
+++ 228_summary_ranges.cpp
@@ -6,7 +6,7 @@ public:
 
         for(int i=0; i<n; ++i)
         {
-            int left = nums[i];
+            int start = nums[i];
             while (i + 1 < n && nums[i + 1] == nums[i] + 1) {
                 i++;
             }
