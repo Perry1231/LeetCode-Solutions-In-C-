e71d59538b596b6060d6620f0f6237596e2f9cd6
Vladyslav Vytrykush <148484877+Perry1231@users.noreply.github.com>
2026-09-06 17:00:41 +0300

    Update 48_rotate_image.cpp - LeetCode Problem 48

--- 48_rotate_image.cpp
+++ 48_rotate_image.cpp
@@ -5,7 +5,7 @@ public:
 
         for(int i=0; i<n; ++i)
         {
-            for(int j=0; j<i+1; ++j)
+            for (int j = i + 1; j < n; ++j)
             {
                 std::swap(matrix[i][j], matrix[j][i]);
             }
