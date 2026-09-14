e4007b29b5e7abbc7ef4b566757b0d41245eeb96
Vladyslav Vytrykush <148484877+Perry1231@users.noreply.github.com>
2026-09-14 20:29:05 +0300

    Update 1486_find_the_distance_value_between_two_arrays.cpp - LeetCode Problem 1486

--- 1486_find_the_distance_value_between_two_arrays.cpp
+++ 1486_find_the_distance_value_between_two_arrays.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
     int findTheDistanceValue(vector<int>& arr1, vector<int>& arr2, int d) {
-        int res=0;
-        for(int i=0; i<arr1.size(); i++)
-        {
+        int res = 0;
+        for (int i = 0; i < arr1.size(); i++) {
             bool valid = true;
-            for(int j=0; j<arr2.size(); j++)
-            {
-            if(std::abs(arr1[i] - arr2[j]) <= d) {res++;
-            valid = false;
-                    break; 
-            }
+            for (int j = 0; j < arr2.size(); j++) {
+                if (std::abs(arr1[i] - arr2[j]) <= d) {
+                    valid = false;
+                    break;
+                }
             }
             if (valid) {
                 res++;
