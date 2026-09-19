48559cb5c046e014293489e019ef310679d7b343
Vladyslav Vytrykush <148484877+Perry1231@users.noreply.github.com>
2026-09-19 16:38:45 +0300

    Add 462_minimum_moves_to_equal_array_elements_ii.cpp - LeetCode Problem 462

---
+++ 462_minimum_moves_to_equal_array_elements_ii.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minMoves2(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int median = nums[nums.size() / 2];
+        int res = 0;
+        
+        for (int num : nums) {
+            res += abs(num - median);
+        }
+        
+        return res;
+    }
+};
